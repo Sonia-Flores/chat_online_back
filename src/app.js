826fa8f630afb80c7ec1f8b8d35183bbe66bda3a
Sonia-Flores <sonia.fmayoral@gmail.com>
2024-04-10 10:53:23 +0200

    Committing changes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 // Creación y configuración de la APP de Express
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // Configuración de rutas
-
-
+app.get("/", (req, res) => {
+  res.send("Ruta dentro del back");
+});
 
 module.exports = app;
